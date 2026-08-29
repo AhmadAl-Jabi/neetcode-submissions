954e3c70efbc6be5ad3279aaac75074e38f241c1
AhmadAl-Jabi <ahm.aljabi@gmail.com>
2026-08-29 01:47:30 -0400

    Update: two-integer-sum - submission-0

--- Data Structures & Algorithms/two-integer-sum/submission-0.py
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -1,14 +1,17 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-
-        complements = {}
-
+        sum_pairings = {}
+        # go through each number and see if target - curr in the hashmap --> if not just append curr 
         for i in range(len(nums)):
-            complement_val = target - nums[i] 
+            curr_value = nums[i]
+            complement = target - curr_value
 
-            if nums[i] in complements:
-                return [complements[nums[i]], i]
+            if complement in sum_pairings:
+                return [sum_pairings[complement], i]
             
-            else:
-                complements[complement_val] = i
+            sum_pairings[curr_value] = i
+
+        # then when we do meet the match (e.g. 3 , 4 target 7) we would reach 4 and see 3 already in hashmap so we return the indices in order anyways 
+
+        # the only issue I see is that you can have dupe entries but that literally doesn't matter since if it pairs with itself you'd just return first_instance, curr_instance and if they DON'T pair then you'd already have stored the first instance and ignore the second instance of it (e.g. [5,5,8] target = 13 --> [0,2]) BUT THIS WOULDN'T EVEN OCCUR BECAUSE WE'RE TOLD IT CAN'T HAPPEN
         
